3211d17e5d409beec8205a03418a6c0c7ef8345c
nijanes97 <nickjanes97@gmail.com>
2019-11-07 18:07:55 -0800

    finished mvp

---
+++ react-redux-app/src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import SpellsList from './components/SpellsList';
+
+function App() {
+  return (
+    <div className="App">
+      <SpellsList />
+    </div>
+  );
+}
+
+export default App;
